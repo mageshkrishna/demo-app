27236fc134a39f5de3e6a8117e743c1544101fc2
mageshkrishna <101549769+mageshkrishna@users.noreply.github.com>
2025-12-16 16:48:33 +0530

    Replace print_hello with query_postgres function

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-def print_hello():
-    print("Hello from the DAG!")
+def query_postgres():
+    hook = PostgresHook(postgres_conn_id="post")
+    conn = hook.get_conn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    version = cursor.fetchone()
+    print("Postgres version:", version)
 
 with DAG(
     dag_id="print_message_dag",
     start_date=datetime(2024, 1, 1),
-    schedule_interval=None,
+    schedule=None,
     catchup=False,
 ) as dag:
 
     print_task = PythonOperator(
         task_id="print_hello_task",
-        python_callable=print_hello,
+        python_callable=query_postgres,
     )
-
-    print_task
