98495ea7c0f88d8e4df06bdbf9427510f8e35ea3
mageshkrishna <101549769+mageshkrishna@users.noreply.github.com>
2026-01-30 20:39:29 +0530

    Update pwd.py

--- pwd.py
+++ pwd.py
@@ -3,13 +3,13 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="print_pwd",
+    dag_id="dbt_debug_only",
     start_date=datetime(2026, 1, 1),
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    print_pwd = BashOperator(
-        task_id="print_pwd",
-        bash_command="pwd"
+    dbt_debug = BashOperator(
+        task_id="dbt_debug",
+        bash_command="dbt debug --project-dir /opt/airflow/"
     )
