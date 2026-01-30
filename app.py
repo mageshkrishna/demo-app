cf27ccf0c76b72d0adad41be599056ab5d4ab9f2
mageshkrishna <101549769+mageshkrishna@users.noreply.github.com>
2026-01-30 20:04:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="dbt_debug_only",
+    dag_id="verify_dbt_project_file",
     start_date=datetime(2026, 1, 1),
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    dbt_debug = BashOperator(
-        task_id="dbt_debug",
-        bash_command="dbt debug --project-dir /opt/airflow/dags"
+    verify = BashOperator(
+        task_id="verify",
+        bash_command="""
+        echo "=== LIST /opt/airflow/dags ==="
+        ls -la /opt/airflow/dags
+        echo
+        echo "=== FILE TYPE ==="
+        file /opt/airflow/dags/dbt_project.yml || echo "file not found"
+        echo
+        echo "=== FILE CONTENT ==="
+        cat /opt/airflow/dags/dbt_project.yml || echo "cannot read file"
+        """
     )
