af6e0cd56dfd631a45d969da9a2de847762d4607
mageshkrishna <101549769+mageshkrishna@users.noreply.github.com>
2026-01-30 20:43:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="print_root_tree_full",
+    dag_id="print_root_tree_structured",
     start_date=datetime(2026, 1, 1),
     schedule_interval=None,
     catchup=False,
@@ -12,8 +12,10 @@ with DAG(
     print_root = BashOperator(
         task_id="print_root_tree",
         bash_command="""
-        echo "===== FULL FILESYSTEM TREE FROM / ====="
-        find / -print 2>/dev/null
-        echo "===== END TREE ====="
+        for dir in / /opt /opt/airflow /home /dataflow /tmp; do
+          echo "===== TREE OF $dir ====="
+          find $dir -maxdepth 3 -print 2>/dev/null
+          echo
+        done
         """
     )
