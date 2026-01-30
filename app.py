fc09374bee7ed7505192c49ef6bb4cf937417b38
mageshkrishna <101549769+mageshkrishna@users.noreply.github.com>
2026-01-30 20:28:17 +0530

    Rename DAG and add find_dbt_project_yml task

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="dbt_debug_only",
+    dag_id="find_dbt_project_yml_opt_airflow",
     start_date=datetime(2026, 1, 1),
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    dbt_debug = BashOperator(
-        task_id="dbt_debug",
-        bash_command="dbt debug --project-dir /opt/airflow/"
+    find_file = BashOperator(
+        task_id="find_dbt_project_yml",
+        bash_command="""
+        echo "===== SEARCHING FOR dbt_project.yml INSIDE /opt/airflow ====="
+        find /opt/airflow/ -type f -name "dbt_project.yml" 2>/dev/null || echo "NOT FOUND"
+        echo "===== SEARCH COMPLETE ====="
+        """
     )
